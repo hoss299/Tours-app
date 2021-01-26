bf2f8d80d374963e4f2befb5bee0527d80aeefd7
Hossam <hossamsaeedg99@yahoo.com>
2021-01-26 22:10:21 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
+// import ShowMoreText from "react-show-more-text";
+
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [tours, setTours] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+
+  const fetchTours = async () => {
+    setIsloading(false);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+    } catch (error) {
+      setIsloading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  const removeItem = (id) => {
+    setTours((oldTours) => {
+      let newTours = oldTours.filter((tour) => tour.id !== id);
+      return newTours;
+    });
+  };
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="title">Our Tours</h1>
+      {tours.map((tour) => {
+        return <Tour key={tour.id} tour={tour} removeItem={removeItem} />;
+      })}
+    </>
   );
 }
+
+const Tour = ({ tour, removeItem }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <article>
+      <img src={tour.image} alt="tour-image" />
+      <footer>
+        <div className="tour-info">
+          <h3>{tour.name}</h3>
+          <h3 className="price">{tour.price} $</h3>
+        </div>
+        {readMore ? tour.info : `${tour.info.substring(0, 200)}...`}
+        <button
+          onClick={() => {
+            setReadMore(!readMore);
+          }}
+          className="more-less-btn"
+        >
+          {readMore ? "show less" : "read More"}
+        </button>
+
+        <button className="remove-btn" onClick={() => removeItem(tour.id)}>
+          Not interested
+        </button>
+      </footer>
+    </article>
+  );
+};
 
 export default App;
